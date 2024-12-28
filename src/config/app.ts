3023161b3a2cb1b6a71ddf77f03a3be3518a6b33
mungsehari <mungse.hari@gmail.com>
2024-12-28 11:32:08 +0530

    add : erorr handler

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,27 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.send("Welcome to the API");
+});
+
+// global error handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
 });
 export default app;
