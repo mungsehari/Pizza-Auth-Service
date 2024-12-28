0cb71774ba0e58d265e8879580947dfdccb9ba06
mungsehari <mungse.hari@gmail.com>
2024-12-28 14:11:54 +0530

    add : test setup

---
+++ app.spec.ts
@@ -0,0 +1,15 @@
+import { calculateDiscount } from "./src/utils";
+import app from "./src/app";
+import request from "supertest";
+
+describe("App", () => {
+  it("should return corrent discount amount", () => {
+    const discount = calculateDiscount(100, 10);
+    expect(discount).toBe(10);
+  });
+
+  it("should return 200 status code", async () => {
+    const response = await request(app).get("/").send();
+    expect(response.status).toBe(200);
+  });
+});
