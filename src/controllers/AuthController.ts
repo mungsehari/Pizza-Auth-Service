825ab4ed9d5f9eba5fc8561a52d723fe7eda524d
mungsehari <mungse.hari@gmail.com>
2025-03-04 10:52:28 +0530

    add auth-controller

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,14 +1,84 @@
-import { Response } from "express";
+import fs from "fs";
+import path from "path";
+import { JwtPayload } from "./../../node_modules/@types/jsonwebtoken/index.d";
+import { NextFunction, Response } from "express";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
+import { Logger } from "winston";
+import { sign } from "jsonwebtoken";
+import createHttpError from "http-errors";
+import { Config } from "../config";
 
 export class AuthController {
-  constructor(private userService: UserService) {
-    this.userService = userService;
-  }
-  async register(req: RegisterUserRequest, res: Response) {
+  constructor(
+    private userService: UserService,
+    private logger: Logger,
+  ) {}
+  async register(req: RegisterUserRequest, res: Response, next: NextFunction) {
     const { firstName, lastName, email, password } = req.body;
-    await this.userService.create({ firstName, lastName, email, password });
-    res.status(201).json();
+    this.logger.info(`new  request to register user:`, {
+      firstName,
+      lastName,
+      email,
+      password: "*****",
+    });
+
+    try {
+      const user = await this.userService.create({
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+      this.logger.info(`User created with id:`, { id: user.id });
+      let privateKey: Buffer;
+
+      try {
+        privateKey = fs.readFileSync(
+          path.join(__dirname, "../../certs/private.pem"),
+        );
+      } catch (error) {
+        const err = createHttpError(
+          500,
+          "Failed to read private key from file",
+        );
+        next(err);
+        return;
+      }
+      const payload: JwtPayload = {
+        sub: String(user.id),
+        role: user.role,
+      };
+
+      const accessToken = sign(payload, privateKey, {
+        algorithm: "RS256",
+        expiresIn: "1h",
+        issuer: "auth-service",
+      });
+      const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
+        algorithm: "HS256",
+        expiresIn: "1y",
+        issuer: "auth-service",
+      });
+
+      res.cookie("accessToken", accessToken, {
+        domain: "localhost",
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60,
+        httpOnly: true,
+      });
+
+      res.cookie("refreshToken", refreshToken, {
+        domain: "localhost",
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60,
+        httpOnly: true,
+      });
+
+      res.status(201).json({ id: user.id });
+    } catch (error) {
+      next(error);
+      return;
+    }
   }
 }
