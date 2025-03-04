dbabd9119418b97b1fcb36e4c0a5fc5216c05807
mungsehari <mungse.hari@gmail.com>
2025-03-04 10:53:15 +0530

    add test

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,29 +1,35 @@
 import request from "supertest";
-import { User } from "../../src/entity/User";
 import { DataSource } from "typeorm";
+
+import { User } from "../../src/entity/User";
 import { AppDataSource } from "../../src/config/data-source";
-import { truncateTables } from "../utils/Index";
 import app from "../../src/app";
+import { Roles } from "../../src/constants";
 
 describe("POST /auth/register", () => {
   let connection: DataSource;
+
   beforeAll(async () => {
     connection = await AppDataSource.initialize();
   });
-  afterEach(async () => {
-    await truncateTables(connection);
+
+  beforeEach(async () => {
+    await connection.dropDatabase();
+    await connection.synchronize();
   });
+
   afterAll(async () => {
     await connection.destroy();
   });
+
   describe("Give all fields", () => {
     it("should return the 201 status code", async () => {
       // Arrange
       const userData = {
-        firstName: "Rakesh",
-        lastName: "K",
-        email: "rakesh@mern.space",
-        password: "password",
+        firstName: "Hari",
+        lastName: "Mungse",
+        email: "hari@email.com",
+        password: "hari",
       };
       // Act
       const response = await request(app as any)
@@ -36,10 +42,10 @@ describe("POST /auth/register", () => {
     it("should return valid json response", async () => {
       // Arrange
       const userData = {
-        firstName: "Rakesh",
-        lastName: "K",
-        email: "rakesh@mern.space",
-        password: "password",
+        firstName: "Hari",
+        lastName: "Mungse",
+        email: "hari@email.com",
+        password: "hari",
       };
       // Act
       const response = await request(app as any)
@@ -72,6 +78,48 @@ describe("POST /auth/register", () => {
       expect(users[0].firstName).toBe(userData.firstName);
       expect(users[0].lastName).toBe(userData.lastName);
       expect(users[0].email).toBe(userData.email);
+    });
+
+    it("should return an id of the created user", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Hari",
+        lastName: "Mungse",
+        email: "hari@email.com",
+        password: "hari",
+      };
+      // Act
+      const response = await request(app as any)
+        .post("/auth/register")
+        .send(userData);
+
+      // Assert
+      const userRepository = connection.getRepository(User);
+
+      await userRepository.save(userData);
+      const users = await userRepository.find();
+      expect(users[0]).toHaveProperty("id");
+      expect(users[0].id).toBeGreaterThan(0);
+    });
+
+    it("should assign a customer role", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Hari",
+        lastName: "Mungse",
+        email: "hari@email.com",
+        password: "hari",
+      };
+      // Act
+      await request(app as any)
+        .post("/auth/register")
+        .send(userData);
+
+      //Assert
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      expect(users[0]).toHaveProperty("role");
+      expect(users[0].role).toBe(Roles.CUSTOMER);
     });
   });
   describe("Fields are Missing  ", () => {});
