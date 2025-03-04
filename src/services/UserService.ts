70510a96cd93e35832bed9a156f2583b0d872775
mungsehari <mungse.hari@gmail.com>
2025-03-04 10:53:05 +0530

    add userservice

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,11 +1,23 @@
 import { Repository } from "typeorm";
-
+import createHttpError from "http-errors";
 import { User } from "../entity/User";
 import { UserData } from "../types";
+import { Roles } from "../constants";
 
 export class UserService {
   constructor(private userRepository: Repository<User>) {}
   async create({ firstName, lastName, email, password }: UserData) {
-    await this.userRepository.save({ firstName, lastName, email, password });
+    try {
+      return await this.userRepository.save({
+        firstName,
+        lastName,
+        email,
+        password,
+        role: Roles.CUSTOMER,
+      });
+    } catch (error) {
+      const err = createHttpError(500, "Failed to store the data in database ");
+      throw err;
+    }
   }
 }
