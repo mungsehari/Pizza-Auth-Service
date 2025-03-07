345b03734dfed0514095257dcc2cb5178c406aca
mungsehari <mungse.hari@gmail.com>
2025-03-07 20:21:00 +0530

    add 401 status code test

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth";
 import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.json());
 
@@ -20,7 +21,7 @@ app.post("/auth/register", async (req, res) => {
 // global error handler
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
   logger.error(err.message);
-  const statusCode = err.statusCode || 500;
+  const statusCode = err.statusCode || err.status || 500;
 
   res.status(statusCode).json({
     errors: [
