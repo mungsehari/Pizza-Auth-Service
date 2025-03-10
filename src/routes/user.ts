8544415b18dbf048adf0ebb288eabb89bd20f399
mungsehari <mungse.hari@gmail.com>
2025-03-10 19:58:28 +0530

    add logger

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,12 +6,13 @@ import { Roles } from "../constants";
 import { UserService } from "../services/UserService";
 import { AppDataSource } from "../config/data-source";
 import { User } from "../entity/User";
+import logger from "../config/logger";
 
 const router = express.Router();
 
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const userController = new UserController(userService);
+const userController = new UserController(userService, logger);
 
 router.post(
   "/",
